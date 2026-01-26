78d44bafdc792c8dd9c86b0b5c0bde20dbd810ba
Aditya0125962 <chelluruaditya5@gmail.com>
2026-01-26 10:10:11 +0530

    playwright config

--- PlayWright/playwright.config.ts
+++ PlayWright/playwright.config.ts
@@ -14,11 +14,11 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: !process.env.CI,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -37,24 +37,24 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-       name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
     //{
-      //name: 'chromium',
-     // use: { ...devices['Desktop Chrome'] },
+      // name: 'Microsoft Edge',
+      //use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //},
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    //{
+      //name: 'firefox',
+      //use: { ...devices['Desktop Firefox'] },
     //},
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
+    /*{
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
